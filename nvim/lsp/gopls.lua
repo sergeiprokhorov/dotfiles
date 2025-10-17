69954185882736b557e727a1ab963c9176fe408c
Sergei Prokhorov <s.prokhorov@protonmail.ch>
2025-10-17 20:06:32 +0200

    Added configuration files for Neovim

---
+++ nvim/lsp/gopls.lua
@@ -0,0 +1,21 @@
+return {
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_markers = { "go.work", "go.mod", ".git" },
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  on_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, silent = true, noremap = true }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  end,
+}
